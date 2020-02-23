6d04ba06208f26fd7bd452cddeb2ce6c053b7da3
Hữu Đăng <huudang@Dangs-MBP.local>
2020-02-23 10:33:37 +0700

    commit-add

---
+++ src/components/SearchResult.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const SearchResult = ({ startupName }) => {
+  return (
+    <View style={styles.nameContainer}>
+      <Text style={styles.startupName}>{startupName}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  nameContainer: {
+    flex: 1,
+    margin: 5,
+    backgroundColor: '#ddd',
+    height: 64,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  startupName: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
+
+export default SearchResult
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, FlatList } from 'react-native'
+import name from '@rstacruz/startup-name-generator'
+
+import SearchBar from './../components/SearchBar'
+import SearchResult from './../components/SearchResult'
+
+const HomeScreen = () => {
+  const [keywordsString, onChangeKeywords] = useState('')
+  const [results, setResults] = useState([])
+
+  const generateStartupNames = () => {
+    const startupNames = name(keywordsString)
+    setResults(startupNames)
+  }
+
+  return (
+    <View style={styles.containerStyle}>
+      <Text style={styles.heading}>Enter some keywords</Text>
+      <Text style={styles.headingTwo}>Separated by space</Text>
+      <SearchBar
+        term={keywordsString}
+        onTermChange={text => onChangeKeywords(text)}
+        onTermSubmit={() => generateStartupNames()}
+      />
+      <FlatList
+        data={results}
+        keyExtractor={(result) => {
+          return result
+        }}
+        renderItem={({ item }) => {
+          return <SearchResult startupName={item} />
+        }}
+        horizontal={false}
+        numColumns={2}
+        style={styles.listStyle}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    marginHorizontal: 15,
+    flex: 1
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+  headingTwo: {
+
+  },
+  listStyle: {
+
+  }
+})
+
+export default HomeScreen
